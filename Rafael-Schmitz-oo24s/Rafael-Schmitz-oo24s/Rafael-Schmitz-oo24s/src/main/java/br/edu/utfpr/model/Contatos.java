cba9465c14cf6e4c4538ce88f7ca8e7eb9c047f5
Rafael <rafasch@live.com>
2019-11-06 20:05:46 -0300

    Create Contatos.java

---
+++ Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/src/main/java/br/edu/utfpr/model/Contatos.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Rafa
+ */
+public class Contatos {
+    
+    private Long id;
+    
+    private String nome;
+    
+    private String telefone;
+    
+    private String email;
+
+    public Contatos() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contatos other = (Contatos) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
