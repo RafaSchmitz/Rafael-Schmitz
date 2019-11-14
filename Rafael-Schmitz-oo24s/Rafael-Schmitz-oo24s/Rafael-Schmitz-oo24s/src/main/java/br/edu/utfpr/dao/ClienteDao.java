6f09abe48e5e803db3871f2b6c61d3b7a9414c7a
Rafael <rafasch@live.com>
2019-11-13 21:25:39 -0300

    Create ClienteDao.java

---
+++ Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/src/main/java/br/edu/utfpr/dao/ClienteDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.dao;
+
+import br.edu.utfpr.model.Cliente;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Rafa
+ */
+public class ClienteDao extends GenericDao<Cliente, Long>{
+
+    public ClienteDao() {
+        super(Cliente.class);
+    }
+    
+    
+    
+}
