1adbbd5da50041766e15604449b8638740c4352a
Rafael <rafasch@live.com>
2019-11-13 21:25:36 -0300

    Create CategoriaDao.java

---
+++ Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/src/main/java/br/edu/utfpr/dao/CategoriaDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.dao;
+
+import br.edu.utfpr.model.Categoria;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Rafa
+ */
+public class CategoriaDao extends GenericDao<Categoria, Long> {
+
+    public CategoriaDao() {
+        super(Categoria.class);
+    }
+
+}
