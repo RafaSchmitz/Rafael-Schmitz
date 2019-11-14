f51559044f4777aba87153608be6eb38979f4682
Rafael <rafasch@live.com>
2019-11-13 21:25:59 -0300

    Create Produto.java

---
+++ Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/src/main/java/br/edu/utfpr/model/Produto.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.model;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import jdk.jfr.Enabled;
+
+/**
+ *
+ * @author Rafa
+ */
+@Entity
+public class Produto implements AbstractModel{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "nome", length = 50, nullable = false)
+    private String nome;
+    
+    @Column(name = "descricao", length = 500, nullable = false)
+    private String descricao;
+    
+    @Column(name = "valor")
+    private Double valor; 
+    
+    @ManyToOne()
+    @JoinColumn(name="categoria_id", referencedColumnName = "id")
+    private Categoria categoria;
+
+    public Produto() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 13 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
