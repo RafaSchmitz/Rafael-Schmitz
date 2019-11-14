ac9a146d45dfc8ac863ea4a4e1be5d71e6313728
Rafael <rafasch@live.com>
2019-11-13 21:25:52 -0300

    Create Cliente.java

---
+++ Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/src/main/java/br/edu/utfpr/model/Cliente.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.model;
+
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Rafa
+ */
+@Entity
+public class Cliente implements AbstractModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 244, nullable = false)
+    private String nome;
+
+    @Column(name = "cpf", length = 11, nullable = false)
+    private String cpf;
+
+    @Column(name = "rg", length = 9, nullable = false)
+    private String rg;
+
+    @Column(name = "numPassaporte", length = 8, nullable = false)
+    private String numPassaporte;
+
+    @OneToMany(mappedBy = "cliente",
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY)
+    private List<Contatos> contatos;
+
+    public Cliente() {
+    }
+
+    public List<Contatos> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contatos> contatos) {
+        this.contatos = contatos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNumPassaporte() {
+        return numPassaporte;
+    }
+
+    public void setNumPassaporte(String numPassaporte) {
+        this.numPassaporte = numPassaporte;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
