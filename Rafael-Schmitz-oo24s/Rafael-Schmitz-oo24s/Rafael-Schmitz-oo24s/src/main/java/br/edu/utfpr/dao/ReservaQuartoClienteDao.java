7251df0f5f8f2440bbbf50e2782efbe9c48ccaf4
Rafael <rafasch@live.com>
2019-11-17 20:14:11 -0300

    Create ReservaQuartoClienteDao.java

---
+++ Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/Rafael-Schmitz-oo24s/src/main/java/br/edu/utfpr/dao/ReservaQuartoClienteDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.dao;
+
+import br.edu.utfpr.model.ReservaQuartoCliente;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Rafa
+ */
+public class ReservaQuartoClienteDao extends GenericDao<ReservaQuartoCliente, Integer>{
+
+    public ReservaQuartoClienteDao() {
+        super(ReservaQuartoCliente.class);
+    }  
+}
