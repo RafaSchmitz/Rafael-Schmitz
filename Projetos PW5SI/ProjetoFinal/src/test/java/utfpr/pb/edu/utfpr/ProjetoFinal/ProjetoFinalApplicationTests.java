d75acc96351375a1118a5c616ac13b30b449f5a8
RafaSchmitz <rafasch@live.com>
2021-04-14 20:47:08 -0300

    InicioProjetoFinal

---
+++ Projetos PW5SI/ProjetoFinal/src/test/java/utfpr/pb/edu/utfpr/ProjetoFinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.pb.edu.utfpr.ProjetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
